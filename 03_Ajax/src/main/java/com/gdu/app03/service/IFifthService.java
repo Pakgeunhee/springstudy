9cf91ef5bc3084d4108a619d3b5fe7fdfde7cf0d
pakgeunhee <pakgeunhee@gmail.com>
2023-04-17 23:19:52 +0900

    [2023-04-17]

--- 03_Ajax/src/main/java/com/gdu/app03/service/IFifthService.java
+++ 03_Ajax/src/main/java/com/gdu/app03/service/IFifthService.java
@@ -1,5 +1,9 @@
 package com.gdu.app03.service;
 
-public interface IFifthService {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.ResponseEntity;
+
+public interface IFifthService {
+   public ResponseEntity<String> papago(HttpServletRequest request);
 }
