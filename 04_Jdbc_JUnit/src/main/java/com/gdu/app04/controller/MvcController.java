e954d35e3462a1a8d553797e3babe363e43bf2e8
pakgeunhee <pakgeunhee@gmail.com>
2023-04-18 19:55:51 +0900

    [2023-04-18]

--- 04_Jdbc_JUnit/src/main/java/com/gdu/app04/controller/MvcController.java
+++ 04_Jdbc_JUnit/src/main/java/com/gdu/app04/controller/MvcController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MvcController {
 	@GetMapping("/")
-	public String welcom() {
+	public String welcome() {
 		return "index";
 	}
 }
